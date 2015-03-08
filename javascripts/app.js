36d7ed0253260e05834ff03bbdce47a9ff1d95d1
Andrew1507 <awesterman1507@gmail.com>
2015-03-08 01:26:58 -0800

    Added the JSON request to app.js to pull my twitter avatar

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,15 @@
 var main = function() {
-  "use strict";
-  console.log("Hello World!");
+    "use strict";
+    
+    var username = "socalslacker";
+    var url = "http://twitcher.steer.me/user/"+ username + "?key=9wfrwzhc";
+    var $profilename = $("<p>").text("Avatar for "+username+":");
+    var $avatar = $("<img>");
+    $.getJSON(url, function (data) {
+        $("main").append($profilename);
+        $avatar.attr("src", data.profile_image_url_https);
+        $("main").append($avatar);
+    });
 };
 
 $(document).ready(main);
