5cfb269306bbef1ce5af39776de130db87cbda07
Andrew1507 <awesterman1507@gmail.com>
2015-03-09 23:12:03 -0700

    Added a form so that for dynamic searches

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,24 @@
 var main = function() {
     "use strict";
-    
-    var username = "socalslacker";
-    var url = "http://twitcher.steer.me/user/"+ username + "?key=9wfrwzhc";
-    var $profilename = $("<p>").text("Avatar for "+username+":");
-    var $avatar = $("<img>");
-    $.getJSON(url, function (data) {
-        $("main").append($profilename);
-        $avatar.attr("src", data.profile_image_url_https);
-        $("main").append($avatar);
-    });
+        
+    $(".submit").on("click", function() {
+        var username,
+            url, 
+            $profilename,
+            $avatar;
+        console.log("Button clicked!");
+        if($(".username").val() !== ""){
+            username = $(".username").val();
+        }
+        url = "http://twitcher.steer.me/user/"+ username + "?key=9wfrwzhc";
+        $profilename = $("<p>").text("Avatar for @"+username+":");
+        $avatar = $("<img>");
+        $.getJSON(url, function (data) {
+            $avatar.attr("src", data.profile_image_url_https);
+            $("main").prepend($avatar);
+            $("main").prepend($profilename);            
+        });
+    });    
 };
 
 $(document).ready(main);
